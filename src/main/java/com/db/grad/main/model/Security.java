b254786786cda78ee1535a185d78ab345f551fe2
Aman Gupta <guptaaman704@gmail.com>
2023-08-08 22:58:22 +0530

    schema changes and user entity issue fixed

--- src/main/java/com/db/grad/main/model/Security.java
+++ src/main/java/com/db/grad/main/model/Security.java
@@ -1,13 +1,17 @@
 package com.db.grad.main.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Security {
 
